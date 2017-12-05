d94328d95ac6bcbdd9cec18688aa4cc4499c876e
Millsionaire <Michael.MillsATX@gmail.com>
2017-12-04 22:25:03 -0600

    Initial Commit
    Initial commit after yo angular install

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contigoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the contigoApp
+ */
+angular.module('contigoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contigoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the contigoApp
+ */
+angular.module('contigoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
